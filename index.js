8c1070bb4f65e62c26c6dcba1a9b8c6cd646d865
Ruoxin Lin <ruoxinlin.ltg@gmail.com>
2019-07-24 01:31:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday,name) {
-  return `Happy 4{holiday}, ${name}!`
+  return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(days,holiday) {
   return `It\'s ${days} days until ${holidays}!`
